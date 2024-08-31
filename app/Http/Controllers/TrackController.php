e4963f59142127303be6001f0dc80a0e48452fd6
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-08-31 22:36:59 +0530

    new content

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -421,7 +421,8 @@ class TrackController extends Controller
             }
         }
 
-        echo "<pre>"; print_r($emailData); die;
+        $uniqueEmailData = array_map('unserialize', array_unique(array_map('serialize', $emailData)));
+        echo "<pre>"; print_r($uniqueEmailData); die;
 
         foreach (array_chunk($emailData, 50) as $batch) {
             foreach ($batch as $data) {
